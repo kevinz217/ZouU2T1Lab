5a0e3f2a844dbd0e5cd714f8d5c6e878b11ac16b
bt_1n3_25 <sample@bths.edu>
2023-09-22 09:08:35 -0400

    cat1 cat2 added

---
+++ src/CatRunner.java
@@ -0,0 +1,15 @@
+public class CatRunner {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Dave", 4, 2.7);
+        cat1.introduce();
+        cat1.printCatInfo();
+        Cat cat2 = new Cat("Sam", 2,3.4);
+        cat2.introduce();
+        cat2.printCatInfo();
+
+
+
+
+
+    }
+}
