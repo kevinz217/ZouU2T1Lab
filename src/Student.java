6765c3af0c56ddfc199ae2af8d0d4430891006f1
Kevin <kevinz217@nycstudents.net>
2023-09-26 01:32:45 -0400

    new method added

--- src/Student.java
+++ src/Student.java
@@ -12,16 +12,16 @@ public class Student {
 
     public void Signin() {
         System.out.println("Hi! My name is " + sName + "and I am signing in!");
-        if (gradeNum .equals 9) {
+        if (gradeNum == 9) {
             System.out.println("I am a freshman!");
         }
-        if (gradeNum = 10) {
+        if (gradeNum == 10) {
             System.out.println("I am a sophomore!");
         }
-        if (gradeNum = 11) {
+        if (gradeNum == 11) {
             System.out.println("I am a junior!");
         }
-        if (gradeNum = 12) {
+        if (gradeNum == 12) {
             System.out.println("I am a senior!");
         }
         System.out.println("ID: " + ID);
